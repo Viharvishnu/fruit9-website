3f6af73ebea9d700957d7cb767126a78fbc6de90
Sai Rohit <sairohit11.s@gmail.com>
2024-08-14 12:02:50 +0530

    Added disc in subscription cards

--- src/modules/home-module/home-subscription/subscription-options.js
+++ src/modules/home-module/home-subscription/subscription-options.js
@@ -3,6 +3,8 @@ const subscriptionData = [
     name: "3 day Subscription",
     small: "Rs 300/-",
     large: "Rs 540/-",
+    ogSmall: "Rs 300/-",
+    ogLarge: "Rs 540/-",
     duration: "3 days",
     description: "We will send you fresh and healthy fruit boxes for 3 days of a week."
   },
@@ -10,6 +12,8 @@ const subscriptionData = [
     name: "Weekly Subscription",
     small: "Rs 510/-",
     large: "Rs 960/-",
+    ogSmall: "Rs 660/-",
+    ogLarge: "Rs 1320/-",
     duration: "6 days",
     description: "We will send you fresh and healthy fruit boxes for 6 days of a week."
   },
@@ -17,6 +21,8 @@ const subscriptionData = [
     name: "Monthly Subscription",
     small: "Rs 2125/-",
     large: "Rs 4000/-",
+    ogSmall: "Rs 2750/-",
+    ogLarge: "Rs 5500/-",
     duration: "25 days",
     description: "We will send you fresh and healthy fruit boxes for 25 days of a month."
   }
@@ -24,7 +30,7 @@ const subscriptionData = [
 
 const SubscriptionOptions = ({ setSubscription }) => {
   const subscriptionDataMapper = (item, index) => {
-    const { description, name, duration, small, large } = item;
+    const { description, name, duration, small, large, ogSmall, ogLarge } = item;
     return (
       <li key={`list-method-${index}`} className="h-auto">
         <label htmlFor={item.name} className="block relative">
@@ -42,11 +48,17 @@ const SubscriptionOptions = ({ setSubscription }) => {
               <p className="text-sm text-gray-700 mt-1 mb-2">{duration}</p>
               <span className="flex items-center my-1 gap-2">
                 <span>Small Box :</span>
-                <span className="bg-green-300 block py-1 px-4 rounded-lg">{small}</span>
+                <span className="flex gap-2 items-center">
+                  {index > 0 && <span className="text-gray-500 line-through">{ogSmall}</span>}
+                  <span className="bg-green-300 block py-1 px-4 rounded-lg">{small}</span>
+                </span>
               </span>
               <span className="flex items-center my-1 gap-2">
                 <span>Large Box :</span>
-                <span className="bg-green-300 block py-1 px-4 rounded-lg">{large}</span>
+                <span className="flex gap-2 items-center">
+                  {index > 0 && <span className="text-gray-500 line-through">{ogLarge}</span>}
+                  <span className="bg-green-300 block py-1 px-4 rounded-lg">{large}</span>
+                </span>
               </span>
               <p className="mt-1 text-gray-700">{description}</p>
             </div>
